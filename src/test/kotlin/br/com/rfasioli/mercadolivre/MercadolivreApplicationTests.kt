805fe4b87349b9bc0861642f3c1c2c85c16059ab
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2022-04-10 20:16:06 -0300

    8 - ktlint and detekt

--- src/test/kotlin/br/com/rfasioli/mercadolivre/MercadolivreApplicationTests.kt
+++ src/test/kotlin/br/com/rfasioli/mercadolivre/MercadolivreApplicationTests.kt
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class MercadolivreApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
