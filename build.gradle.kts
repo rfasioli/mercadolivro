5364d38227edad550ceeccbc2389367ffcde9bb2
Rodrigo Fasioli <7225579+rfasioli@users.noreply.github.com>
2022-04-10 20:30:47 -0300

    8 configure lint (#9)
    * 4 controllers customer
    
    * 4 - fix build test for jvm 17
    
    * 8 - ktlint and detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.6"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.20"
-	kotlin("plugin.spring") version "1.6.20"
+    id("org.springframework.boot") version "2.6.6"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.20"
+    kotlin("plugin.spring") version "1.6.20"
+    id("io.gitlab.arturbosch.detekt") version "1.0.0.RC6-4"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 group = "br.com.rfasioli"
@@ -12,28 +14,39 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	val javaToolchains = project.extensions.getByType<JavaToolchainService>()
-	javaLauncher.set(javaToolchains.launcherFor {
-		languageVersion.set(JavaLanguageVersion.of(17))
-	})
+    useJUnitPlatform()
+    val javaToolchains = project.extensions.getByType<JavaToolchainService>()
+    javaLauncher.set(
+        javaToolchains.launcherFor {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    )
+}
+
+detekt {
+    version = "1.0.0.RC6-4"
+    defaultProfile {
+        input = "$projectDir/src/main/kotlin"
+        config = "$projectDir/config/detekt/style-config.yml" // Code style rules file.
+        filters = ".*/res/.*,.*build/.*"
+    }
 }
