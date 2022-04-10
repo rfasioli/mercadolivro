e5acc4e3c2dd79f3bb68b4ea3a672a215cb8c6b4
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2022-04-10 19:12:21 -0300

    4 controllers customer

---
+++ src/main/kotlin/br/com/rfasioli/mercadolivre/controller/request/PostCustomerRequest.kt
@@ -0,0 +1,6 @@
+package br.com.rfasioli.mercadolivre.controller.request
+
+data class PostCustomerRequest(
+    val name: String,
+    val email: String
+)
---
+++ src/main/kotlin/br/com/rfasioli/mercadolivre/controller/request/PutCustomerRequest.kt
@@ -0,0 +1,6 @@
+package br.com.rfasioli.mercadolivre.controller.request
+
+data class PutCustomerRequest(
+    val name: String,
+    val email: String
+)
