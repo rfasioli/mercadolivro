d0137cce8407d1349e27ff882b478b44a56398d3
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2022-04-10 21:56:06 -0300

    5 - Customer Service

--- src/main/kotlin/br/com/rfasioli/mercadolivro/controller/mapper/CustomerMapper.kt
+++ src/main/kotlin/br/com/rfasioli/mercadolivro/controller/mapper/CustomerMapper.kt
@@ -4,15 +4,16 @@ import br.com.rfasioli.mercadolivro.controller.request.PostCustomerRequest
 import br.com.rfasioli.mercadolivro.controller.request.PutCustomerRequest
 import br.com.rfasioli.mercadolivro.model.CustomerModel
 
-fun PostCustomerRequest.toModel(id: String): CustomerModel =
+fun PostCustomerRequest.toModel(id: String? = null): CustomerModel =
     CustomerModel(
         id,
         name,
         email
     )
 
-fun PutCustomerRequest.mergeWithModel(model: CustomerModel): CustomerModel {
-    model.name = name
-    model.email = email
-    return model
-}
+fun PutCustomerRequest.toModel(id: String): CustomerModel =
+    CustomerModel(
+        id,
+        name,
+        email
+    )
