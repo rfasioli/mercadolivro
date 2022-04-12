d68f479b5646c646de601a24371f541469738e97
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2022-04-12 00:40:44 -0300

    Book repository, model and migration

--- src/main/kotlin/br/com/rfasioli/mercadolivro/repository/BookRepository.kt
+++ src/main/kotlin/br/com/rfasioli/mercadolivro/repository/BookRepository.kt
@@ -3,5 +3,4 @@ package br.com.rfasioli.mercadolivro.repository
 import br.com.rfasioli.mercadolivro.model.BookModel
 import org.springframework.data.repository.CrudRepository
 
-interface BookRepository: CrudRepository<BookModel, Int> {
-}
+interface BookRepository : CrudRepository<BookModel, Int>
