89687b39c7a424b9bceed5f3fe526abb7db70205
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2022-04-30 17:09:00 -0300

    [wip] controller and entity

---
+++ src/main/kotlin/br/com/rfasioli/mercadolivro/controller/apidoc/PurchaseControllerOpenApi.kt
@@ -0,0 +1,12 @@
+package br.com.rfasioli.mercadolivro.controller.apidoc
+
+import br.com.rfasioli.mercadolivro.controller.request.PostPurchaseRequest
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+
+@Tag(name = "Purchase", description = "this API provide methods for operations with purchases")
+interface PurchaseControllerOpenApi {
+
+    @Operation(summary = "Create a purchase on Mercado Livro")
+    fun createPurchase(request: PostPurchaseRequest)
+}
---
+++ src/main/kotlin/br/com/rfasioli/mercadolivro/model/PurchaseModel.kt
@@ -0,0 +1,51 @@
+package br.com.rfasioli.mercadolivro.model
+
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EntityListeners
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
+import javax.persistence.ManyToMany
+import javax.persistence.ManyToOne
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@Entity(name = "book")
+@EntityListeners(AuditingEntityListener::class)
+data class PurchaseModel(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    val customer: CustomerModel,
+
+    @ManyToMany
+    @JoinTable(
+        name = "purchase_book",
+        joinColumns = [JoinColumn(name = "purchase_id")],
+        inverseJoinColumns = [JoinColumn(name = "book_id")]
+    )
+    val books: List<BookModel>,
+
+    @Column
+    val nfe: String,
+
+    @Column
+    val price: BigDecimal,
+
+    @Column(name = "created_at")
+    @CreatedDate
+    val createdAt: LocalDateTime,
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    val updatedAt: LocalDateTime
+)
