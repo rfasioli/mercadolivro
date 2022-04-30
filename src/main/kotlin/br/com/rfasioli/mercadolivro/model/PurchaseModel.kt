d0e03df25b5af176276f0feddef0a9fa52df11d4
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2022-04-30 17:25:01 -0300

    db migration tables for purchase

--- src/main/kotlin/br/com/rfasioli/mercadolivro/model/PurchaseModel.kt
+++ src/main/kotlin/br/com/rfasioli/mercadolivro/model/PurchaseModel.kt
@@ -16,7 +16,7 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
-@Entity(name = "book")
+@Entity(name = "purchase")
 @EntityListeners(AuditingEntityListener::class)
 data class PurchaseModel(
     @Id
