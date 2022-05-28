411d74ade815c04e5a171a09e49caa2a17e0e790
Rodrigo Fasioli <rodrigo.fasioli@gmail.com>
2022-05-28 16:48:32 -0300

    Customer Repository test

---
+++ src/test/kotlin/br/com/rfasioli/mercadolivro/IntegratedTest.kt
@@ -0,0 +1,9 @@
+package br.com.rfasioli.mercadolivro
+
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@ExtendWith(MockKExtension::class)
+interface IntegratedTest
